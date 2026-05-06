f3d1cdcc416050e89fd48fe97e964495603cf47e
rozaq <auliavera203@gmail.com>
2026-05-06 23:06:09 +0700

    refactor: remove webpack configuration for geojson file handling

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.geojson$/,
-      type: "json",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
